e83ee6d098d67395239ba6f0e8bfc535eff0ca88
Barry Lagerweij <blagerweij@gmail.com>
2019-02-12 01:19:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,18 @@ const s3 = new AWS.S3({ region: 'us-east-1' });
 
 exports.handler = (event, context, callback) => {
     const request = event.Records[0].cf.request;
-    const origin = request.origin.s3;
+    const origin = request.origin.s3; // for now we only support S3 origins
     const options = querystring.parse(request.querystring);
-    const width = Math.min(options.width || 1024, 1024);
-    const height = Math.min(options.height || 768, 768);
-    const convert = spawn('convert', ['-', '-resize', `${width}x${height}>`, '-quality', '80', '-']);
-    s3.getObject({
+    const width = Math.min(options.width || 1024, 1024); // max width 1024
+    const height = Math.min(options.height || 768, 768); // max height 768
+    const s3stream = s3.getObject({
         Bucket: origin.domainName.slice(0, -17), // remove '.s3.amazonaws.com' to get bucket
-        Key: `${origin.path}${request.uri.substring(1)}`
-    }).createReadStream()
-        .on('error', error => context.fail(error))
-        .pipe(convert.stdin);
+        Key: `${origin.path}${request.uri.substring(1)}` // key is URL excluding the leading '/'
+    }).createReadStream().on('error', error => context.fail(error));
+    const convert = spawn('convert', ['-', '-resize', `${width}x${height}>`, '-quality', '80', '-']);
+    s3stream.pipe(convert.stdin); // pipe output from S3 into ImageMagick convert
     const chunks = [];
-    convert.stdout.on('data', chunk => chunks.push(chunk));
+    convert.stdout.on('data', chunk => chunks.push(chunk)); // collect chunks of ImageMagick convert output
     convert.on('close',(code) => {
         if (code === 0) {
             context.succeed({
